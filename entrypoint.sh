82a128caed9e10b70ba017d364eefa85db048846
Carlos Anaya <59860385+anayagaray98@users.noreply.github.com>
2022-04-14 14:25:01 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ set -e
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-# DJANGO_SUPERUSER_PASSWORD=superuser python manage.py createsuperuser --noinput --username=admin  --email=anaya.garay98@gmail.com
 
 
 gunicorn backend.wsgi:application --bind 0.0.0.0:8000
